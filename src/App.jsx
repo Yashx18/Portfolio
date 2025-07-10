19e311f63b1c9bdb45d3c1f9affa0fd504ae6c84
ken <yash20004x17@gmail.com>
2025-07-10 05:30:13 +0000

    Started Working on the Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Page from "./layout/page";
+function App() {
+  return (
+    <main className="w-full h-dvh flex items-center justify-center bg-[#08090a]">
+      <Page />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/layout/page.jsx
@@ -0,0 +1,7 @@
+const Page = () => {
+  return (
+    <div>This is th emain page where all the components will be assembled.</div>
+  );
+};
+
+export default Page;
