e1bfea71965ad0596c7ce8bdf8683b0b85d3bc62
ken <yash20004x17@gmail.com>
2025-07-15 07:27:56 +0000

    Analytics enabled

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Page from "./layout/page";
+import { Analytics } from "@vercel/analytics/react";
 function App() {
   return (
     <main className="w-full h-full flex items-center justify-center bg-[#08090a] ">
       <Page />
+      <Analytics />
     </main>
   );
 }
